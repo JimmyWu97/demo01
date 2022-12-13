494df076f5b2ca5349d18bb9d493124b51fa78f4
JimmyWu97 <m13166992379@163.com>
2022-12-13 19:31:37 +0800

    测试

---
+++ src/main/java/Dugujiujian.java
@@ -0,0 +1,14 @@
+/**
+ * @Description:
+ * @Author: wjm
+ * @Date: 2022/12/13 37
+ */
+public class Dugujiujian {
+    public static void main(String[] args) {
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+    }
+}
